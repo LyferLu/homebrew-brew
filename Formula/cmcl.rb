11b0bd7753e59f015e9a1e9434b2002ca92aa1ba
mrshiehx <3553413882@qq.com>
2023-02-26 20:19:18 +0800

    CMCL v2.1.1

--- Formula/cmcl.rb
+++ Formula/cmcl.rb
@@ -1,9 +1,9 @@
 class Cmcl < Formula
   desc "A Launcher for Minecraft Java Edition Running On The Console"
   homepage "https://github.com/MrShieh-X/console-minecraft-launcher"
-  url "https://github.com/MrShieh-X/console-minecraft-launcher/releases/download/2.1/cmcl.tar.gz"
-  sha256 "03dd85daf397344877d63e1e6ad6e7b143ddbdcd6b14ecfb3724412df6638265"
-  version "2.1"
+  url "https://github.com/MrShieh-X/console-minecraft-launcher/releases/download/2.1.1/cmcl.tar.gz"
+  sha256 "34f271c748b10546b781ae5e3c63a93a82c17c4b70c47a16b65a0a85597ef0a1"
+  version "2.1.1"
   license "GPL-3.0"
   def install
     bin.install "cmcl"
