73164a80ca20093ae07b52c9089323b35b377ff7
MrShieh-X <3553413882@qq.com>
2023-02-27 13:30:13 +0800

    added mirror for cmcl

--- Formula/cmcl.rb
+++ Formula/cmcl.rb
@@ -2,6 +2,7 @@ class Cmcl < Formula
   desc "A Launcher for Minecraft Java Edition Running On The Console"
   homepage "https://github.com/MrShieh-X/console-minecraft-launcher"
   url "https://github.com/MrShieh-X/console-minecraft-launcher/releases/download/2.1.1/cmcl.tar.gz"
+  mirror "https://gitee.com/MrShiehX/console-minecraft-launcher/releases/download/2.1.1/cmcl.tar.gz"
   sha256 "34f271c748b10546b781ae5e3c63a93a82c17c4b70c47a16b65a0a85597ef0a1"
   version "2.1.1"
   license "GPL-3.0"
